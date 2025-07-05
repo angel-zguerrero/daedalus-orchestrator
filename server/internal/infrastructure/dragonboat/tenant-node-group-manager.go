6a8aca1add8d61e8d7041b80507f20c02419fdff
Angel Zambrano <angel.zguerrero@gmail.com>
2025-07-05 01:06:20 -0400

    feat: add StartTenantNodes function for initializing tenant nodes

---
+++ server/internal/infrastructure/dragonboat/tenant-node-group-manager.go
@@ -0,0 +1,28 @@
+package dragonboat
+
+import (
+	"deadalus-orch/server/internal/infrastructure/db"
+	"deadalus-orch/server/internal/pkg/config"
+)
+
+func StartTentantNodes(
+	replicaID uint64,
+	selfMember Member,
+	initialMembers []Member,
+	join bool,
+	roles []NodeRole,
+	pathProvider db.PathProvider,
+) error {
+	MaxTenants := config.GlobalConfiguration.MaxTenants
+	for shardID := 0; shardID < MaxTenants; shardID++ {
+		tenantMember := Member{
+			IP:   selfMember.IP,
+			Port: config.GlobalConfiguration.TenantPortLowerBound + shardID,
+		}
+		_, err := InitTenantNode(uint64(shardID+MasterShardID), replicaID, tenantMember, initialMembers, join, roles, pathProvider)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
