f893be4c31a4f524a7256fda5b4451e2d5c11efd
Angel Zambrano <angel@multivende.com>
2026-06-15 17:36:54 -0400

    chore: bump version to v1.0.3 in default footer component

--- web-admin/src/app/layout/default-layout/default-footer/default-footer.component.ts
+++ web-admin/src/app/layout/default-layout/default-footer/default-footer.component.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from '@coreui/angular';
   styleUrls: ['./default-footer.component.scss']
 })
 export class DefaultFooterComponent extends FooterComponent {
-  readonly version = 'v1.0.2';
+  readonly version = 'v1.0.3';
 
   constructor() {
     super();
