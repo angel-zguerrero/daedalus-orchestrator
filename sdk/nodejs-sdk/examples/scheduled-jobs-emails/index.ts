32ed0a9effdaa4355aaf917f02ff2474081e568d
Angel Zambrano <angel@multivende.com>
2026-09-09 16:03:58 -0400

    feat: add unique codes to one-off and recurring scheduled job examples

--- sdk/nodejs-sdk/examples/scheduled-jobs-emails/index.ts
+++ sdk/nodejs-sdk/examples/scheduled-jobs-emails/index.ts
@@ -87,6 +87,7 @@ async function main() {
     };
 
     const oneOffJob = await sdk.createOneOffScheduledJob({
+      code: "welcome-job-1",
       tenantCode: tenantCode,
       targetType: 'exchange',
       targetCode: 'email-events',
@@ -110,6 +111,7 @@ async function main() {
     };
 
     const recurringJob = await sdk.createRecurringScheduledJob({
+      code: "weekly-reports-job-1",
       tenantCode: tenantCode,
       targetType: 'queue',
       targetCode: 'weekly-reports',
